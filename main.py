9c2e21bda92cfe9ca20e0381b48d9c638a23a634
Pavlo <pavelparkhomchuk@gmail.com>
2021-09-15 13:49:21 +0300

    Updated validation of data. Added error handler.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 import random
 import string
+from marshmallow import validate
+from webargs import fields
+from webargs.flaskparser import use_kwargs
 
 app = Flask(__name__)
+
+
+@app.errorhandler(422)
+@app.errorhandler(400)
+def handle_error(err):
+    headers = err.data.get("headers", None)
+    messages = err.data.get("messages", ["Invalid request."])
+    if headers:
+        return jsonify({"errors": messages}), err.code, headers
+    else:
+        return jsonify({"errors": messages}), err.code
 
 
 @app.route("/")
@@ -12,17 +25,15 @@ def hello_world():
 
 
 @app.route("/password")
-def generate_password():
-    length = request.args.get('length', 10)
-
-    if not length.isdigit():
-        return 'ERROR: not an integer'
-
-    length = int(length)
-
-    if not 8 <= length <= 100:
-        return 'ERROR: out of range'
-
+@use_kwargs({
+    "length": fields.Int(
+        # required=True,
+        missing=100,
+        validate=[validate.Range(min=1, max=999)]
+    )},
+    location="query"
+)
+def generate_password(length):
     return ''.join(
         random.choices(
             string.ascii_lowercase + string.ascii_uppercase,
